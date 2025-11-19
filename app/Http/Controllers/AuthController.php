14e50cde95c78a509b685307c5d952d9066c2750
Dyunodawae <natnatdmamangan@gmail.com>
2025-11-19 20:06:27 +0800

    Functions for register and login (success)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 
 class AuthController extends Controller
 {
@@ -14,11 +18,22 @@ class AuthController extends Controller
         return view('authentication.login'); #login
     }
 
-    function performRegister(){
+    function performRegister(Request $request){ #performRegister name dati ng func
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email'     => 'required|email|unique:users',
+            'password'  => 'required|min:6|confirmed' #used in laravel 11
+        ]);
 
+        User::create([
+            'name' => $request->name,
+            'email'     => $request->email,
+            'password'  => Hash::make($request->password)
+        ]);
+        return redirect()->route('login.form')->with('success','Registration successfull!');
     }
     
-    function performLogin(){
+    function performLogin(Request $request){ #performLogin name dati ng func
         
     }
 }
